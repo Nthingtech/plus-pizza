e1e54d3dfbe4e5fac7e45d0dd6eaf04d08a19089
Fernando <store.nthing@gmail.com>
2026-06-30 22:55:15 -0300

    tests

--- src/main/java/br/nthing/pizza/PizzaResource.java
+++ src/main/java/br/nthing/pizza/PizzaResource.java
@@ -2,9 +2,6 @@ package br.nthing.pizza;
 
 import br.nthing.pizza.exceptions.DuplicatePizzaNameException;
 import br.nthing.utils.TextUtil;
-import io.quarkus.arc.profile.IfBuildProfile;
-import io.quarkus.runtime.StartupEvent;
-import jakarta.enterprise.event.Observes;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -18,20 +15,6 @@ import java.util.List;
 
 @Path("/pizza")
 public class PizzaResource {
-
-    @Transactional
-    @IfBuildProfile("dev")
-    public void init(@Observes StartupEvent event) {
-        var pizza1 = new Pizza();
-        pizza1.name = "Quatro Queijos";
-        pizza1.description = "Provolone, Mussarela, Catupiry e Parmesão";
-        pizza1.persist();
-
-        var pizza2 = new Pizza();
-        pizza2.name = "Queijos";
-        pizza2.description = "Vários queijos kkkkk";
-        pizza2.persist();
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
--- src/test/java/br/nthing/PizzaResourceTest.java
+++ src/test/java/br/nthing/PizzaResourceTest.java
@@ -2,32 +2,24 @@ package br.nthing;
 
 import br.nthing.delivery.Location;
 import br.nthing.pizza.Pizza;
-import br.nthing.pizza.PizzaResource;
+import br.nthing.pizza.Pizza_;
 import br.nthing.store.Store;
 import br.nthing.store.Store_;
 import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 class PizzaResourceTest {
-    @Inject
-    PizzaResource pizzaResource;
 
     @BeforeEach
     @Transactional
     public void beforeAll() {
+        Pizza_.repo().deleteAll();
         Store_.repo().deleteAll();
         var store = new Store();
         store.name = "Plus Pizza";
@@ -35,18 +27,15 @@ class PizzaResourceTest {
         store.location = Location.current();
         store.persist();
 
-    }
+        var pizza1 = new Pizza();
+        pizza1.name = "Quatro Queijos";
+        pizza1.description = "Provolone, Mussarela, Catupiry e Parmesão";
+        pizza1.persist();
 
-    @Test
-    void testFindNearestStore() {
-        //GIVEN
-        var location = Location.current();
-
-        //WHEN
-        var store = Store_.repo().findByNearestLocation(location);
-
-        //THEN
-        assertNotNull(store);
+        var pizza2 = new Pizza();
+        pizza2.name = "Queijos";
+        pizza2.description = "Vários queijos kkkkk";
+        pizza2.persist();
 
     }
 
